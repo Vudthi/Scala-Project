5fe38da71aa31ae885e45d1854186830dc5e1d0f
Vudthi <34526897+Vudthi@users.noreply.github.com>
2018-06-12 08:30:11 +0700

    Second Project

---
+++ ClassAndObject.scala
@@ -0,0 +1,15 @@
+class Student
+{
+    def Welcome(name: String, id: Int, grade: String)
+    {
+        println("Welcome back Mr/Miss "+name+"\nYour ID is "+id+"\nYour grade for this semester is "+grade);
+    }
+}
+object demo
+{
+    def main(args: Array[String])
+    {
+        val s= new Student();
+        s.Welcome("Vudthi", 168, "A");
+    }
+}        
